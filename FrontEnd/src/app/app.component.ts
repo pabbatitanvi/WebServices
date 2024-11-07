feff084bd84dbb5ded115aa17f3dc681e546aa0f
pabbatitanvi <pabbatitanvi@gmail.com>
2024-11-07 10:46:10 -0500

    front end

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,16 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FrontPageComponent } from "./front-page/front-page.component";
-import { DisplayComponentComponent } from './display-component/display-component.component';
-import { InfoShareFormComponent } from './info-share-form/info-share-form.component';
-import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FrontPageComponent, DisplayComponentComponent, InfoShareFormComponent, NavigationBarComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'FrontEnd';
