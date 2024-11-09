ab35bd1116c1af0160e446724c42ded37f1b6473
pabbatitanvi <pabbatitanvi@gmail.com>
2024-11-09 14:52:43 -0500

    changes

--- FrontEnd/get-out-there/src/app/app.routes.ts
+++ FrontEnd/get-out-there/src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { UserScreenComponent } from './user-screen/user-screen.component';
 import { HomeScreenComponent } from './home-screen/home-screen.component';
 import { FriendScreenComponent } from './friend-screen/friend-screen.component';
 import { EventScreenComponent } from './event-screen/event-screen.component';
+import { LoginScreenComponent } from './login-screen/login-screen.component';
 
 export const routes: Routes = [
     {path:'', redirectTo:'home', pathMatch:'full'},//This means that when the path is empty after the 4200 in the url, it redirects to the /home url, loading the home screen
+    {path:'login', component: LoginScreenComponent},
     {path:'home', component: HomeScreenComponent},
     {path: 'userprofile', component: UserScreenComponent},
     {path: 'friends', component:FriendScreenComponent},
