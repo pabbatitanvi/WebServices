c59e1df8c303431d26875bbb8cc53eae370d36d7
4DL <148650143+4d-exe@users.noreply.github.com>
2024-11-16 12:57:58 -0500

    bruh

--- FrontEnd/get-out-there/src/app/app.component.ts
+++ FrontEnd/get-out-there/src/app/app.component.ts
@@ -3,11 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { LoginScreenComponent } from './login-screen/login-screen.component';
 import { HomeScreenComponent } from './home-screen/home-screen.component';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationBarComponent, LoginScreenComponent, HomeScreenComponent],
+  imports: [RouterOutlet, NavigationBarComponent, LoginScreenComponent, HomeScreenComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
