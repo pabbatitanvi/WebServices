461d14f65222d909ccaf64e5903b34b1e6895bf3
Samantha Groner <114039769+Sasheanna@users.noreply.github.com>
2024-11-16 21:35:08 -0500

    Hard-coded some events in

--- FrontEnd/get-out-there/src/app/event-screen/event-screen.component.ts
+++ FrontEnd/get-out-there/src/app/event-screen/event-screen.component.ts
@@ -9,15 +9,13 @@ import { NavigationBarComponent } from "../navigation-bar/navigation-bar.compone
   styleUrl: './event-screen.component.css'
 })
 export class EventScreenComponent {
-  
+  public events: any = [
+    {"name" : "EVENT 1", "id" : 1},
+    {"name" : "EVENT 2", "id" : 2} ,
+    {"name" : "EVENT 3", "id" : 3}, 
+    {"name" : "EVENT 4", "id" : 4},
+  ]
 }
 
 
-export class event {
-  name: String;
-
-  constructor(eventName: String){
-    this.name = eventName;
-  }
-}
 
