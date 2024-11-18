bd8fc5679b656c41a5ecb0c6490707ef83fd6e6c
Samantha Groner <114039769+Sasheanna@users.noreply.github.com>
2024-11-18 10:42:01 -0500

    Generates based on events. Working on aligning.
    I'm about to delete all the replaced comments

--- FrontEnd/get-out-there/src/app/event-screen/event-screen.component.ts
+++ FrontEnd/get-out-there/src/app/event-screen/event-screen.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { NavigationBarComponent } from "../navigation-bar/navigation-bar.component";
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-event-screen',
   standalone: true,
-  imports: [NavigationBarComponent],
+  imports: [NavigationBarComponent, NgFor, NgIf],
   templateUrl: './event-screen.component.html',
   styleUrl: './event-screen.component.css'
 })
 export class EventScreenComponent {
   public events: any = [
-    {"name" : "EVENT 1", "id" : 1},
+    {"name" : "EVENT 1", "location" : "museum", "id" : 1},
     {"name" : "EVENT 2", "id" : 2} ,
     {"name" : "EVENT 3", "id" : 3}, 
     {"name" : "EVENT 4", "id" : 4},
