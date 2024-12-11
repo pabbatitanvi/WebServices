1b0e7ebfa53ed62486102f5674339514dc4f9aef
pabbatitanvi <pabbatitanvi@gmail.com>
2024-12-10 21:16:38 -0500

    something

--- FrontEnd/get-out-there/src/app/user-post-page/user-post-page.component.ts
+++ FrontEnd/get-out-there/src/app/user-post-page/user-post-page.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationBarComponent } from "../navigation-bar/navigation-bar.component";//this imports the nav bar component into this file allowing its use here
+import { NgFor, NgIf, CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { GetDataService } from '../../../services/get-data.service';
+import {ObjectId} from 'mongodb';
+import { FormsModule } from '@angular/forms';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+
 
 @Component({
   selector: 'app-user-post-page',
   standalone: true,
-  imports: [],
+  imports: [NavigationBarComponent, NgFor, NgIf, CommonModule, FormsModule, SidebarComponent],
   templateUrl: './user-post-page.component.html',
   styleUrl: './user-post-page.component.css'
 })
